9415900c4c3d8648b5be201c5affa40643c5977e
Aethar <elliott.ashby88@gmail.com>
2023-06-16 04:01:13 +0100

    Cli: added cli control

---
+++ src/config.rs
@@ -0,0 +1,45 @@
+use std::path::PathBuf;
+use serde::{Deserialize, Serialize};
+use confy;
+
+#[derive(Serialize, Deserialize)]
+struct Config {
+    wallpaper_folders: Vec<PathBuf>,
+}
+
+impl ::std::default::Default for Config {
+    fn default() -> Self {
+        Self {
+            wallpaper_folders: vec![],
+        }
+    }
+}
+
+pub fn check_config(config_path: PathBuf, verbose: bool) -> Option<PathBuf> {
+    let _configuration: Config = confy::load_path(&config_path).unwrap();
+    if verbose {
+        println!("Config path: {:?}", &config_path);
+    }
+    Some(config_path)
+}
+
+pub fn add_folder(addpath: PathBuf, config_path: PathBuf) {
+    let mut configuration: Config = confy::load_path(&config_path).unwrap();
+    configuration.wallpaper_folders.push(addpath);
+    confy::store_path(&config_path, configuration).unwrap();
+}
+
+pub fn remove_folder(rmpath: PathBuf, config_path: PathBuf) {
+    let mut configuration: Config = confy::load_path(&config_path).unwrap();
+    let index = configuration.wallpaper_folders.iter().position(|x| *x == rmpath).unwrap();
+    configuration.wallpaper_folders.remove(index);
+    confy::store_path(&config_path, configuration).unwrap();
+}
+
+pub fn list_folders(config_path: PathBuf) {
+    let configuration: Config = confy::load_path(&config_path).unwrap();
+    for folder in configuration.wallpaper_folders {
+        println!("{:?}", folder);
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,73 @@
-// Hitsuki is a commandline tool for dynamic time based wallpapers in x11 based
-// window managers. It is written in rust and uses the x11 crate to interact
-// with the x11 server.
-
-
+/// Hitsuki is a commandline tool for dynamic time based wallpapers in x11 
+/// based window managers. It is written in rust and uses the ffmpeg to 
+/// interact with the x11 server and set the background.
 use std::path::PathBuf;
 use clap::{Parser, Subcommand};
+use lazy_static::lazy_static;
 
-#[derive(Parser)]
-#[command(version = "0.1.0", author = "Elliott A. <elliott.ashby88@gmail.com>")]
-struct Args {
+pub mod config;
+
+lazy_static! {
+    static ref DEFAULT_CONFIG_PATH: PathBuf = {
+        let mut path = dirs::config_dir().unwrap();
+        path.push("hitsuki");
+        path.push("config.toml");
+        path
+    };
 }
 
+#[derive(Parser)]
+#[command(version, author, about, long_about = None)]
+struct Cli {
+    /// Sets a custom config file.
+    /// Default: $HOME/.config/hitsuki/config.toml
+    #[arg(short, long, value_name = "FILE", default_value = DEFAULT_CONFIG_PATH.to_str().unwrap())]
+    config: Option<PathBuf>,
 
+    #[command(subcommand)]
+    command: Option<Commands>,
+
+    /// List all wallpaper folders
+    #[arg(short, long)]
+    list: bool,
+
+    /// Verbose output
+    #[arg(short, long)]
+    verbose: bool,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Add new wallpaper folder
+    Add {
+        #[arg(short, long, value_name = "PATH")]
+        path: PathBuf,
+    },
+
+    /// Remove wallpaper folder
+    Remove {
+        #[arg(short, long, value_name = "PATH")]
+        path: PathBuf,
+    },
+}
 
 fn main() {
-    println!("Hello, world! pog");
+    let cli = Cli::parse();
+    let config_path = cli.config.unwrap();
+
+    match &cli.command {
+        Some(Commands::Add { path }) => {
+            config::add_folder(path.clone(), config_path);
+        }
+        Some(Commands::Remove { path }) => {
+            config::remove_folder(path.clone(), config_path);
+        }
+        None => {
+            if cli.list {
+                config::list_folders(config_path);
+            } else {
+                config::check_config(config_path, cli.verbose);
+            }
+        }
+    }
 }
